c5ce6216bf38cc8163804889e62099920a7e22f0
Daniel <danielmjgmatos@outlook.com>
2022-03-21 10:35:49 +0000

    Update Comentarios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" v-if="!['erro', 'adminPage'].includes($route.name)">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" v-if="!['erro', 'adminPage'].includes($route.name)"> <!-- Faz com que a navbar e o footer mais embaixo, nao aparecam nas paginas de erro e admin -->
             <div class="container px-4 px-lg-5">
                 <router-link to="/" class="navbar-brand">Ana Marques</router-link>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
